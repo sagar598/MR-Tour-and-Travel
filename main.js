fc649ea0997ea427f3a0e2012dc17b833100354b
Anas Ahmed <imanas559@gmail.com>
2024-01-31 03:14:40 +0530

    feat: Added header/navbar, footer, and hero section

---
+++ main.js
@@ -0,0 +1,68 @@
+
+
+// Function to initialize the navbar
+function initializeNavbar() {
+    const button = document.querySelector('[data-collapse-toggle]');
+    const menu = document.querySelector('#mobile-menu-2');
+    const navLinks = document.querySelectorAll('#mobile-menu-2 a');
+    const menuIcon = document.querySelector('.menu-icon');
+    const closeIcon = document.querySelector('.close-icon');
+  
+    button.addEventListener('click', function () {
+        const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+        this.setAttribute('aria-expanded', !expanded);
+        menu.classList.toggle('hidden');
+        menuIcon.classList.toggle('hidden');
+        closeIcon.classList.toggle('hidden');
+    });
+  
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            button.setAttribute('aria-expanded', 'false');
+            menu.classList.add('hidden');
+            menuIcon.classList.remove('hidden');
+            closeIcon.classList.add('hidden');
+        });
+    });
+  }
+  
+  // Function to fetch and insert HTML content
+  function fetchAndInsert(url, placeholderId, callback) {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(placeholderId).innerHTML = html;
+            if (callback) {
+                callback(); // Execute the callback function after inserting HTML
+            }
+        })
+        .catch(error => console.error(`Error fetching ${url}:`, error));
+  }
+  
+  // Load the header HTML
+  document.addEventListener("DOMContentLoaded", function () {
+    fetchAndInsert('/header.html', 'header-placeholder', function () {
+        // After loading the header, initialize the navbar
+        initializeNavbar();
+  
+        // Add event listener to change navbar styles on scroll
+        const navbar = document.querySelector('header nav');
+        window.addEventListener('scroll', function () {
+            if (window.scrollY > 50) {
+                // Scrolled down, add styles
+                navbar.classList.remove('bg-transparent', 'text-white');
+                navbar.classList.add('bg-white', 'text-customBlue');
+            } else {
+                // At the top, remove styles
+                navbar.classList.remove('bg-white', 'text-customBlue');
+                navbar.classList.add('bg-transparent', 'text-white');
+            }
+        });
+    });
+  });
+  
+  // Load the footer HTML
+  document.addEventListener("DOMContentLoaded", function () {
+    fetchAndInsert('/footer.html', 'footer-placeholder');
+  });
+  
